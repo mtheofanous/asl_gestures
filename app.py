c3190e82a34155fa4257833d57cb89277638db20
Martheo <theofanousmario@gmail.com>
2024-07-08 00:06:34 +0200

    comment sklearn

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import os
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score, classification_report
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.metrics import accuracy_score, classification_report
 import numpy as np
 import cv2
 import joblib
